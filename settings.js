ec5b9ada7b5c9a0b9f15c9809ff6e78b4ffa50c7
senshamantha <132114850+senshamantha@users.noreply.github.com>
2023-04-29 07:25:51 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -56,7 +56,7 @@ global.dragonemoji = '🧩' // Menu emoji
 global.footer = '[ ◉ ʀᴇᴅ-ᴅʀᴀɢᴏɴ-ᴍᴅ ]' //
 
 //database
-global.premium = ['++94726013156'] //ur premium numbers
+global.premium = ['+94726013156'] //ur premium numbers
 
 //other
 global.sessionName = 'session'
